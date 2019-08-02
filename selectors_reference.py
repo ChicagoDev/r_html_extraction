3335826cb1628ff98bbb5710737d8f2592ae2506
Blake Gideon <Blake@Gideon.email>
2019-08-01 22:09:41 -0500

    Created Selectors for Featured Reviews and Histogram Stars

--- selectors_reference.py
+++ selectors_reference.py
@@ -18,3 +18,39 @@
 # info_values = [_.get_text().strip() for _ in information_dl.find_all('dd')]
 
 # Version History: Url may be tied up within some JavaScript
+
+# """
+# Obtaining the rating histogram info.
+# """
+# import re
+# num_re = re.compile('[0-9]+')
+# stars = [5, 4, 3, 2, 1]
+#
+# ratings_bar_graph = parsed_church.find('figure', {'class': 'we-star-bar-graph'})
+# ratings_bars = ratings_bar_graph.find_all('div', {'class': 'we-star-bar-graph__bar__foreground-bar'})
+#
+# bar_attributes = [_.attrs for _ in ratings_bars]
+#
+# star_rating_percentages = [re.search(num_re, _['style']).group(0) for _ in bar_attributes]
+#
+# print(star_rating_percentages)
+# zip(stars, star_rating_percentages)
+
+
+##
+# User Reviews
+#
+## User Names
+#
+# users = [_.get_text().strip() for _ in finra_app.find_all('span', {'class': 'we-customer-review__user'})]
+#
+## Review Body
+#
+# reviews = [_.p.get_text() for _ in finra_app.find_all('blockquote', {'class': 'we-truncate we-truncate--multi-line
+# we-truncate--interactive ember-view we-customer-review__body'})]
+#
+## Featured Review Stars
+#
+#featured_review_stars = finra_app.find_all('figure', {'class': 'we-star-rating ember-view we-customer-review__rating we-star-rating--large'})
+# stars = [_.attrs['aria-label'][0] for _ in featured_review_stars]
+#
